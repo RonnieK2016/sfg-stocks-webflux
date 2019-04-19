f3f94223104db98ec1a085faf83ab0d3365e035e
SergeA <ronnieo@gmail.com>
2019-04-19 12:02:40 -0400

    Initial Commit

---
+++ src/main/java/com/example/udemy/sfg/sfgstockswebflux/SfgStocksWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy.sfg.sfgstockswebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgStocksWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgStocksWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udemy/sfg/sfgstockswebflux/SfgStocksWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.udemy.sfg.sfgstockswebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgStocksWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
