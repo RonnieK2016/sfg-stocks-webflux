283bf8778b232b17d83eb769998ef96312d4cfc4
SergeA <ronnieo@gmail.com>
2019-04-19 12:34:10 -0400

    Added QuoteHandler

---
+++ src/main/java/com/example/udemy/sfg/sfgstockswebflux/web/QuoteHandler.java
@@ -0,0 +1,33 @@
+package com.example.udemy.sfg.sfgstockswebflux.web;
+
+import com.example.udemy.sfg.sfgstockswebflux.model.Quote;
+import com.example.udemy.sfg.sfgstockswebflux.services.QuoteGeneratorService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Component
+public class QuoteHandler {
+
+    private final QuoteGeneratorService quoteGeneratorService;
+
+    public QuoteHandler(QuoteGeneratorService quoteGeneratorService) {
+        this.quoteGeneratorService = quoteGeneratorService;
+    }
+
+    Mono<ServerResponse> getQuotes(ServerRequest serverRequest) {
+        int number = Integer.parseInt(serverRequest.queryParam("size").orElse("10"));
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100))
+                        .take(number), Quote.class);
+    }
+}
